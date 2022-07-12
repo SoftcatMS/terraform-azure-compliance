6ec70a3c6f6f3062a283c7d81e7ecb09ea323bc5
kevinbasoftcat <kevinba@softcat.com>
2022-07-12 16:12:19 +0100

    Adjusted display name to include Softcat as a prefix

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ resource "azurerm_subscription_policy_assignment" "regulatory_compliance" {
   name                 = "Azure Security Benchmark"
   policy_definition_id = "/providers/Microsoft.Authorization/policySetDefinitions/1f3afdf9-d0c9-4c3d-847f-89da613e70a8"
   subscription_id      = data.azurerm_subscription.current.id
-  display_name         = "Azure Security Benchmark"
+  display_name         = "Softcat - Azure Security Benchmark"
   description          = "The Azure Security Benchmark initiative represents the policies and controls implementing security recommendations defined in Azure Security Benchmark v2, see https://aka.ms/azsecbm. This also serves as the Azure Security Center default policy initiative. You can directly assign this initiative, or manage its policies and compliance results within Azure Security Center. (Applied by Softcat Managed Azure Deployment)"
 }
