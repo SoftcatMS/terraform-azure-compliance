cbe1cf603d5ac05862f9d60675d98bc384553abc
Nielsen Pierce <nielsenpi@softcat.com>
2022-09-06 18:40:16 +0100

    Update QC test

--- tests/basic/azure-inspec-tests/controls/qc.rb
+++ tests/basic/azure-inspec-tests/controls/qc.rb
@@ -3,5 +3,5 @@
 # Test values
 
 describe azure_policy_assignments do
-  its('names') { should include('Azure Security Benchmark') }
+  its('names') { should include('Softcat-ASB-652f2afd-d7ca-4ffb-977e-de679adc4b03') }
 end
